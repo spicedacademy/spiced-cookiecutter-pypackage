9152a062c4186fc8e0f23839b6c90753fd0a266e
Paul Wlodkowski <pawlodkowski@gmail.com>
2020-02-03 11:48:27 +0100

    switched order of imports to make conf.py work

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -23,9 +23,11 @@ import sys
 
 import sphinx_rtd_theme
 
+sys.path.insert(0, os.path.abspath('..'))
+
 import {{ cookiecutter.project_slug }}
 
-sys.path.insert(0, os.path.abspath('..'))
+
 
 # -- General configuration ---------------------------------------------
 
