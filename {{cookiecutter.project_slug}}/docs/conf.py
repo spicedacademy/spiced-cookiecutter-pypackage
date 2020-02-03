b9c1f062e34ff6b4765c839926acc87d6c1ce6c8
Paul Wlodkowski <pawlodkowski@gmail.com>
2020-02-03 11:32:05 +0100

    changed sphinx theme

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -20,9 +20,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+import sphinx_rtd_theme
 
 import {{ cookiecutter.project_slug }}
+
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ---------------------------------------------
 
@@ -32,7 +35,7 @@ import {{ cookiecutter.project_slug }}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -158,6 +161,3 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
